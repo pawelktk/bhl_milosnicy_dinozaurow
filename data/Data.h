14ad4de84d709616ded1719c24ab3b605742918c
Łukasz Łysy <s184761@o365.student.pg.edu.pl>
2021-04-09 21:33:34 +0200

    jakiesdane

--- data/Data.h
+++ data/Data.h
@@ -1,7 +1,20 @@
 #ifndef DATA_H
 #define DATA_H
-
+//przyjmujemy że domyślnie jednostka energii to W
 class Data{
+private:
+    int maksMoc = 10000;
+    int mocGrzalki = 6000;
+    int mocOgrzewaniaPomieszczen = [
+        1000,
+        1000,
+        1500,
+        1500,
+        2000,
+        2000,
+        3000
+    ];
+public:
     
 };
 
