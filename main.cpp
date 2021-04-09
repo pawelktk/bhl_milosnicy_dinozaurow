5e612ae565314c7e515652cd1b9855bb4007651e
Łukasz Łysy <s184761@o365.student.pg.edu.pl>
2021-04-09 18:33:13 +0200

    test

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include "lib/test.h"
 
 using namespace std;
 
 int main(){
-    cout<<"hello world";
+    A a;
+    a.a();
     return 0;
 }
---
+++ test.cpp
@@ -0,0 +1,6 @@
+#include "lib/test.h"
+#include <iostream>
+
+void A::a(){
+    std::cout<<'a';
+}
